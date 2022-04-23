02f16a4f12c0fbdd324ed62814df6a27357b6db0
nishanbe <Msjammsjam1!>
2022-04-24 02:59:10 +1000

    Add 2 new pages to the navigation

--- inc_nav.php
+++ inc_nav.php
@@ -4,5 +4,7 @@
     <li <?php if ($name == "advisors") echo 'class="current"'; ?>><a href="advisors.php"><span class="glyphicon glyphicon-star"></span> Advisors</a></li>
     <li <?php if ($name == "samples") echo 'class="current"'; ?>><a href="samples.php"><span class="glyphicon glyphicon-comment"></span> Sample Advice</a></li>
     <li <?php if ($name == "subscribe") echo 'class="current"'; ?>><a href="subscribe.php"><span class="glyphicon glyphicon-shopping-cart"></span> Subscribe Now</a></li>
+    <li <?php if ($name == "locations") echo 'class="current"'; ?>><a href="locations.php"><span class="glyphicon glyphicon-map-marker"></span> Locations</a></li>
+    <li <?php if ($name == "testimonials") echo 'class="current"'; ?>><a href="testimonials.php"><span class="glyphicon glyphicon-check"></span> Testimonials</a></li>
   </ul>
 </nav>
